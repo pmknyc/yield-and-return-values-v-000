964be5baa298fbb6f871f0d4a8c52a24db4a8677
Paula McKinley <pmknyc@optimum.net>
2018-10-12 18:00:08 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -8,7 +8,7 @@ def hello(array)
   end
 end
 
-binding.pry
+
 # call the method and give a block to execute
 hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 #return value of above block is the "Hi, #{name}" output
@@ -16,7 +16,7 @@ hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 # but in version at top the return values aren't captured/saved
 
 # this revised method shovels return values of the yield
-# into a new array when the method is run 
+# into a new array when the method is run
 def hello_save(array)
   i = 0
   collection = []
